97d3e9b792ceb225c0f51643a8a725744f6f2d00
Christian Morales <christianmoralescalani@gmail.com>
2019-08-17 21:32:44 -0400

    Commit Final

--- build/src/models/UsuariosModels.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const Usuarioschema = new mongoose_1.Schema({
-    usuario: String,
-    googleID: String,
-    contrasena: String
-});
-exports.default = mongoose_1.model('Usuarios', Usuarioschema);
